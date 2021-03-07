94ba1d2d7ff3d2a580ff89bcfb3a0e43ee1cbe1e
autopp <autopp.inc@gmail.com>
2021-03-07 21:53:01 +0900

    feat: add webhook endpoint for line bot

--- index.ts
+++ index.ts
@@ -1,9 +1,26 @@
 import express from 'express'
+import line from '@line/bot-sdk'
+
+function loadEnv(name: string): string {
+  const v = process.env[name]
+  if (v === undefined) {
+    throw new Error(`cannot load ${name} from env`)
+  }
+
+  return v
+}
 
 const app = express()
-app.get('/', (_, res: express.Response<{ message: string }>) => {
-  res.json({ message: "hello world" })
-}).listen
+app.get('/', (req, res) => {
+  console.log(req.body)
+  res.status(200)
+})
+
+const middleware = line.middleware({
+  channelAccessToken: loadEnv('CHANNEL_ACCESS_TOKEN'),
+  channelSecret: loadEnv('CHANNEL_SECRET'),
+})
+app.post('/webhook', middleware, )
 
 const port = process.env.PORT || 5000
 app.listen(port, () => {
